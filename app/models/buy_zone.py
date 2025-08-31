68bad7ab8ea46e8e484d8bf13ed9b31323e8b0cb
Diego Urbina <diegourbina.dev@gmail.com>
2025-08-30 21:39:37 -0500

    fix: se agregó status para soft delete

--- app/models/buy_zone.py
+++ app/models/buy_zone.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Numeric, DateTime, String
+from sqlalchemy import Column, Integer, Numeric, DateTime, String, Boolean
 from sqlalchemy.sql import func
 from ..config.db import Base
 
@@ -9,6 +9,7 @@ class BuyZone(Base):
     buy_price = Column(Numeric(precision=18, scale=3))
     user = Column(String(100))
     create_at = Column(DateTime, server_default=func.now())
+    status = Column(Boolean, default=True, server_default="true")
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
